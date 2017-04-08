ea64ceeaab2b91a642522818fe09cb21b8e1923a
Trenton Mcleod <Boanerges@lawn-128-61-57-158.lawn.gatech.edu>
2017-04-08 17:51:01 -0400

    new files

---
+++ basicCard.js
@@ -0,0 +1,21 @@
+// Create a new GitHub repository, 
+// named Flashcard-Generator or something similar.
+// Clone this to your local drive.
+// Create a BasicCard constructor. 
+// It should accept front and back arguments.
+// Create a ClozeCard constructor. 
+// It should accept text and cloze arguments.
+
+console.log("Hello admin")
+const weather = require('weather-js');
+const fs = require("fs");
+
+class BasicCard {
+    constructor(userName, location) {
+        this.userName = userName;
+        this.location = location;
+    }
+
+}
+
+module.exports = WeatherAdmin;
